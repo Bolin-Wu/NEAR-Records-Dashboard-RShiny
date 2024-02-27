88aecb244b6afafa63475bfdbf9659060cba55a9
Bolin Wu <bolin.wu1026@gmail.com>
2024-02-27 11:56:49 +0100

    fix the bullet list and newline

--- R/clean_wdfile.R
+++ R/clean_wdfile.R
@@ -4,7 +4,7 @@ library(here)
 
 # read data ---------------------------------------------------------------
 
-tbs_database <-  read_excel("data/Troubleshooting_database.xlsx",sheet = "record", guess_max = 21474836)
+data <-  read_excel("data/Troubleshooting_database.xlsx",sheet = "record", guess_max = 21474836)
 # tbs_harmonization <- read_docx("data/Troubleshooting_harmonization.docx")
 
 
--- app.R
+++ app.R
@@ -1,55 +1,65 @@
 library("here")
-source("R/clean_wdfile.R")
+library("DT")
+library("tidyverse")
+library("readxl")
+
+# read data ---------------------------------------------------------------
+
+data <-  read_excel("data/Troubleshooting_database.xlsx",sheet = "record", guess_max = 21474836)
+# tbs_harmonization <- read_docx("data/Troubleshooting_harmonization.docx")
+
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("NEAR Records Troubleshooting"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("database", "Select Database:", choices = unique(tbs_database$Database)),
-      textInput("variable", "Search Variable:", placeholder = "Type to search...")
+  titlePanel("NEAR troubleshooting records"),
+  fluidRow( 
+    column(
+      width = 12,
+      selectInput("database", "Select Database:", choices = unique(data$Database))
     ),
-    mainPanel(
-      tableOutput("description_table")
+    column(
+      width = 12,
+      textInput("variable", "Search Variable:", placeholder = "Type to search...")
     )
+  ),
+  column(
+    width = 12,
+    DTOutput("description_table")
   )
 )
 
 # Define server logic
 server <- function(input, output, session) {
-  
   # Reactive expression to filter data based on database and variable search
   filtered_data <- reactive({
     req(input$database)
-    
+
     # Filter by database
-    filtered <- tbs_database %>%
+    filtered <- data %>%
       filter(Database == input$database)
-    
+
     # If variable search is not empty, filter by variable
     if (input$variable != "") {
       filtered <- filtered %>%
         filter(str_detect(tolower(Variable), tolower(input$variable)))
     }
-    
+
     return(filtered)
   })
-  
+
   # Render description table
-  output$description_table <- renderTable({
+  output$description_table <- renderDT({
     filtered_data <- filtered_data()
-    
-    # If no records found, return NULL
+
+    # If no records found, return an empty data table
     if (nrow(filtered_data) == 0) {
-      return(NULL)
+      return(data.frame()) # Return empty data frame
     }
-    
+
     # Display all descriptions and sources if no variable search is made
-    if (input$variable == "") {
-      return(filtered_data[, c("Variable","Description", "Source")])
-    } else {
-      return(filtered_data[, c("Variable", "Description", "Source")])
-    }
+    datatable(filtered_data[, c("Variable", "Description", "Source")],
+      options = list(dom = "t", paging = TRUE, ordering = TRUE), escape = FALSE
+    )
   })
 }
 
